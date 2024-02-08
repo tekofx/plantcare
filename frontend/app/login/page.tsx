6001d0fb417eb32e05ec1f52c993456a87e93bb7
tekofx <tekofxx@gmail.com>
2024-02-08 19:06:43 +0100

    Added separation

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,20 +1,20 @@
 'use client';
-import { Button, Fieldset, PasswordInput, Stack, TextInput, Title } from '@mantine/core';
+import { Button, Container, Fieldset, PasswordInput, Stack, TextInput, Title } from '@mantine/core';
 import { useState } from 'react';
 
 export default function Page() {
   const [value, setValue] = useState('');
 
   return (
-    <>
+    <Container style={{ paddingTop: 20 }}>
       <Title>Login</Title>
-      <Fieldset legend="Personal information">
+      <Fieldset>
         <Stack>
           <TextInput label="Username" placeholder="Your name" />
           <PasswordInput label="Password" placeholder="Password" />
           <Button variant="filled">Login</Button>
         </Stack>
       </Fieldset>
-    </>
+    </Container>
   );
 }
