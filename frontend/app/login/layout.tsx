3870a9a7428ce842b6a0c70d4f46164a45987a90
tekofx <tekofxx@gmail.com>
2024-02-08 10:05:22 +0100

    fix

--- frontend/app/login/layout.tsx
+++ frontend/app/login/layout.tsx
@@ -1,13 +1,12 @@
-import { Container } from '@mantine/core';
 
 export default function DashboardLayout({
-    children, // will be a page or nested layout
-  }: {
-    children: React.ReactNode
-  }) {
-    return (
-      <div>
-        {children}
-      </div>
-    )
-  }
+  children, // will be a page or nested layout
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
