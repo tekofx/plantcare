69f1bd00f1f957ce44fee53a4cf0060675ece2e8
tekofx <tekofxx@gmail.com>
2024-02-09 11:08:31 +0100

    Get plants

--- frontend/app/home/page.tsx
+++ frontend/app/home/page.tsx
@@ -1,8 +1,33 @@
 import { Title } from '@mantine/core';
+import axios from 'axios';
 
-export default function HomePage() {
+import { cookies } from 'next/headers';
+
+async function getPlants() {
+  const cookieStore = cookies();
+  const token = cookieStore.get('TOKEN').value;
+  const res = await axios.get('http://localhost:3001/plants', { headers: { Authorization: `Bearer ${token}` } });
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  if (res.status !== 200) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.data;
+}
+export default async function HomePage() {
+  const plants = await getPlants();
+
   return (
     <>
+      {plants.map((plant) => (
+        <div key={plant.id}>
+          <h1>{plant.name}</h1>
+          <p>{plant.description}</p>
+        </div>
+      ))}
       <Title>My Plants</Title>
     </>
   );
